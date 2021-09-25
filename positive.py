c9b9c642335ccc27043a1a3821961f0e17c5059f
Harish024 <91045615+Harish024@users.noreply.github.com>
2021-09-25 21:11:05 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,13 @@
+
+myList = [12,-7,5,64,-14]
+print("Output :" )
+for num in myList:
+  if num >= 0 :
+    print(num)
+    
+    
+myList = [12,14,-95,3]
+print("Output :" )
+for num in myList:
+  if num >= 0 :
+    print(num)
